3163610525e74eab17306dfa4fffe1550d868c86
yanzifeng3 <yanzifeng3@jd.com>
2022-04-10 14:45:50 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+class FileListPlugin {
+    constructor(options) {
+
+    }
+    apply(compiler) {
+        compiler.plugin('emit', function (compilation, callback) {
+            // Create a header string for the generated file:
+            var filelist = 'In this build:\n\n';
+        
+            // Loop through all compiled assets,
+            // adding a new line item for each filename.
+            for (var filename in compilation.assets) {
+              filelist += '- ' + filename + '\n';
+            }
+        
+            // Insert this list into the Webpack build as a new file asset:
+            compilation.assets['filelist.md'] = {
+              source: function () {
+                return filelist;
+              },
+              size: function () {
+                return filelist.length;
+              }
+            };
+        
+            callback();
+          });
+    }
+}
+
+module.exports = FileListPlugin;
